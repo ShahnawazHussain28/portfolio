4e767cec699c57ff3882e2f7778516cf3573a4cb
Shahnawaz Hussain <shahnawazhussain2802@gmail.com>
2025-12-20 18:12:22 +0530

    fix: add relevant skills

--- app/data/skills.js
+++ app/data/skills.js
@@ -3,111 +3,112 @@ import {
   SiNextdotjs,
   SiNodedotjs,
   SiTypescript,
-  SiJavascript,
   SiTailwindcss,
-  SiMongodb,
   SiExpress,
   SiPython,
   SiGit,
   SiDocker,
-  SiAmazonwebservices,
   SiFirebase,
   SiGraphql,
-  SiRedux,
   SiJest,
-  SiSocketdotio
-} from 'react-icons/si';
-import { TbApi } from 'react-icons/tb';
+  SiSocketdotio,
+  SiUnity,
+  SiPostgresql,
+  SiOpencv,
+  SiBlender,
+  SiPrisma,
+  SiGithubactions,
+  SiCplusplus,
+} from "react-icons/si";
+import { TbApi, TbBrandCSharp } from "react-icons/tb";
 
 export const skills = [
   {
     name: "React",
     icon: SiReact,
-    category: "Frontend"
   },
   {
     name: "Next.js",
     icon: SiNextdotjs,
-    category: "Frontend"
   },
   {
     name: "Node.js",
     icon: SiNodedotjs,
-    category: "Backend"
+  },
+  {
+    name: "OpenCV",
+    icon: SiOpencv,
   },
   {
     name: "TypeScript",
     icon: SiTypescript,
-    category: "Language"
   },
   {
-    name: "JavaScript",
-    icon: SiJavascript,
-    category: "Language"
+    name: "C++",
+    icon: SiCplusplus,
   },
   {
     name: "Tailwind",
     icon: SiTailwindcss,
-    category: "Frontend"
   },
   {
-    name: "MongoDB",
-    icon: SiMongodb,
-    category: "Database"
+    name: "Prisma",
+    icon: SiPrisma,
+  },
+  {
+    name: "PostgreSQL",
+    icon: SiPostgresql,
   },
   {
     name: "Express",
     icon: SiExpress,
-    category: "Backend"
   },
   {
     name: "Python",
     icon: SiPython,
-    category: "Language"
   },
   {
     name: "Git",
     icon: SiGit,
-    category: "Tools"
   },
   {
     name: "Docker",
     icon: SiDocker,
-    category: "DevOps"
   },
   {
-    name: "AWS",
-    icon: SiAmazonwebservices,
-    category: "Cloud"
+    name: "Github Actions",
+    icon: SiGithubactions,
+  },
+  {
+    name: "Unity",
+    icon: SiUnity,
+  },
+  {
+    name: "C#",
+    icon: TbBrandCSharp,
+  },
+  {
+    name: "Blender",
+    icon: SiBlender,
   },
   {
     name: "Firebase",
     icon: SiFirebase,
-    category: "Backend"
   },
   {
     name: "GraphQL",
     icon: SiGraphql,
-    category: "Backend"
-  },
-  {
-    name: "Redux",
-    icon: SiRedux,
-    category: "Frontend"
   },
   {
     name: "Jest",
     icon: SiJest,
-    category: "Testing"
   },
   {
     name: "WebSocket",
     icon: SiSocketdotio,
-    category: "Backend"
   },
   {
     name: "REST API",
     icon: TbApi,
-    category: "Backend"
   },
 ];
